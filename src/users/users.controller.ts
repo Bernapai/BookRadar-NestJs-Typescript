19a46015467fe3f3f7e50c734b0f27f9b2f9c36f
Bernapai <juanemeunier@gmail.com>
2025-05-15 17:51:44 -0300

    Add userController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Put,
+  Delete,
+  Param,
+  Body,
+  Post
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UpdateUserDto } from './dtos/updateUser.dto';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersService.createUser(createUserDto);
+  }
+
+  @Get()
+  async findAllUsers(): Promise<User[]> {
+    return await this.usersService.findAllUsers();
+  }
+
+  @Get(':id')
+  async findUserById(@Param('id') id: number): Promise<User> {
+    return await this.usersService.findUserById(id);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return await this.usersService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: number): Promise<void> {
+    return await this.usersService.deleteUser(id);
+  }
+}
