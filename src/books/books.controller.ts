9de5e20870153970cac889d8d26fce82c956a41f
Bernapai <juanemeunier@gmail.com>
2025-05-16 17:59:00 -0300

    Add BooksController

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { GetBooksDto } from './books.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) { }
+
+  @Get()
+  async getBooks(@Query('q') query: string): Promise<GetBooksDto[]> {
+    return this.booksService.getAll(query);
+  }
+
+  // GET /books/:id
+  @Get(':id')
+  async getBookById(@Param('id') id: string): Promise<GetBooksDto> {
+    return this.booksService.getOne(id);
+  }
+}
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [BooksService],
-  controllers: [BooksController]
+  controllers: [BooksController],
 })
-export class BooksModule {}
+export class BooksModule { }
