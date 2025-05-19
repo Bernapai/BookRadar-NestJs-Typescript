cb911b0d04585dba93ce5e813b7a2505dc13a995
Bernapai <juanemeunier@gmail.com>
2025-05-19 17:13:42 -0300

    Changes on AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/users.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      secret: 'JksIJ093jJS', // Cambiar por una variable de entorno
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule { }
