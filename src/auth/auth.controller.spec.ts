9d32678809f7cd82cd64467a26be7d9dd02cd88b
Bernapai <juanemeunier@gmail.com>
2025-05-25 17:59:23 -0300

    Add authController testing

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,34 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { loginUser } from './dtos/loginUser.dto';
+
+describe('AuthController', () => {
+  let controller: AuthController;
+  let authService: Partial<AuthService>;
+
+  beforeEach(async () => {
+    authService = {
+      login: jest.fn(),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [{ provide: AuthService, useValue: authService }],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+  });
+
+  it('debería llamar a authService.login y devolver el token', async () => {
+    const loginDto: loginUser = { name: 'user', password: '1234' };
+    const mockResponse = { access_token: 'token123' };
+
+    (authService.login as jest.Mock).mockResolvedValue(mockResponse);
+
+    const result = await controller.login(loginDto);
+
+    expect(authService.login).toHaveBeenCalledWith(loginDto);
+    expect(result).toEqual(mockResponse);
+  });
+});
